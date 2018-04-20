4945bb1ff9a20a1edca75665a9a4aa0e7aebd9e7
huongvt789 <huongvt52@wru.vn>
2018-04-20 13:10:33 +0700

    update observer

---
+++ Observer.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: viva
+ * Date: 4/19/18
+ * Time: 5:16 PM
+ */
+abstract class GameObserver
+{
+    abstract function update(GameSubject $subject);
+}
+
+abstract class GameSubject
+{
+    abstract function attach(GameObserver $observer);
+
+    abstract function detach(GameObserver $observer);
+
+    abstract function notify();
+}
+
+class PatternObserver extends GameObserver
+{
+    public function __construct()
+    {
+    }
+
+    function update(GameSubject $subject)
+    {
+        echo $subject->getFavorite();
+    }
+}
+
+class PatternSubject extends GameSubject
+{
+    private $favorite = null;
+    private $observer = array();
+
+    public function __construct()
+    {
+    }
+
+    function attach(GameObserver $observer)
+    {
+        $this->observer[] = $observer;
+    }
+
+    function detach(GameObserver $observer)
+    {
+        foreach ($this->observer as $okey => $avue) {
+            if ($avue == $observer) {
+                unset($this->observer[$okey]);
+            }
+        }
+    }
+
+    function updateContentLike($newLike)
+    {
+        $this->favorite = $newLike;
+        $this->notify();
+    }
+
+    function getFavorite()
+    {
+        return $this->favorite;
+    }
+
+    function notify()
+    {
+        foreach ($this->observer as $new) {
+            $new->update($this);
+        }
+    }
+}
+
+$patternOb = new PatternObserver();
+$patternSu = new PatternSubject();
+$patternSu->attach($patternOb);
+$patternSu->updateContentLike("Oki nhe");
